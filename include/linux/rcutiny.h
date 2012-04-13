e7aac33fff26e148c936c2f720646391ba3f7d24
Paul E. McKenney <paul.mckenney@linaro.org>
2012-04-13 12:54:22 -0700

    Revert "rcu: Precompute RCU_FAST_NO_HZ timer offsets"
    reverted from commit 728932f56ce0860b67cf6f5fd72fcbcf3f8b2d1b

--- include/linux/rcutiny.h
+++ include/linux/rcutiny.h
@@ -91,9 +91,8 @@ static inline void exit_rcu(void)
 {
 }
 
-static inline int rcu_needs_cpu(int cpu, unsigned long *delta_jiffies)
+static inline int rcu_needs_cpu(int cpu)
 {
-	*delta_jiffies = ULONG_MAX;
 	return 0;
 }
 
@@ -102,9 +101,8 @@ static inline int rcu_needs_cpu(int cpu, unsigned long *delta_jiffies)
 extern void exit_rcu(void);
 int rcu_preempt_needs_cpu(void);
 
-static inline int rcu_needs_cpu(int cpu, unsigned long *delta_jiffies)
+static inline int rcu_needs_cpu(int cpu)
 {
-	*delta_jiffies = ULONG_MAX;
 	return rcu_preempt_needs_cpu();
 }
 
